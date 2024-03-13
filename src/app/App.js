70f0770e7349c29975ff518a7633d9333905d165
Jose M. Gomez <gomezjosem@live.com>
2024-03-13 23:07:29 +0000

    added dynamic routes for each article and author to our router

--- src/app/App.js
+++ src/app/App.js
@@ -19,7 +19,9 @@ const router = createBrowserRouter(
     <Route path="/" element={ <Root/> }>
       <Route path="about" element={ <About/> }/>
       <Route path="sign-up" element={ <SignUp/> } />
-      <Route path="articles" element={ <Articles/> } />  
+      <Route path="articles" element={ <Articles/> }/>
+      <Route path="articles/:title" element={ <Article/> }/>
+      <Route path="authors/:name" element={ <Author/> }/>
       <Route path="categories" element={ <Categories/> } />  
       <Route path="profile" element={ <Profile/> } />  
     </Route>
