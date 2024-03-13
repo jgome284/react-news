150d7a7f828a2256945f5033bf329b1544216254
Jose M. Gomez <gomezjosem@live.com>
2024-03-13 21:15:18 +0000

    created root route

--- src/app/App.js
+++ src/app/App.js
@@ -10,22 +10,21 @@ import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
 
-import { Route } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 import "./App.css";
 
-const router = 
-  /* Wrap this Root Route to create Router here */
-  <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
-  </Route>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={ <Root/> }>
+      {/* Add Routes here! */}
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <>
-      { /* Replace below and add Router Provider*/}
-      <p>REPLACE ME WITH A ROUTER PROVIDER</p>
-    </>
+    <RouterProvider router={router}/>
   );
 }
 
