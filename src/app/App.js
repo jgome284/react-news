ee5fccdb168ab72a7771e22027043eddc3c40fcf
Jose M. Gomez <gomezjosem@live.com>
2024-03-14 02:01:55 +0000

    added category nested route

--- src/app/App.js
+++ src/app/App.js
@@ -22,7 +22,9 @@ const router = createBrowserRouter(
       <Route path="articles" element={ <Articles/> }/>
       <Route path="articles/:title" element={ <Article/> }/>
       <Route path="authors/:name" element={ <Author/> }/>
-      <Route path="categories" element={ <Categories/> } />  
+      <Route path="categories" element={ <Categories/> }>  
+        <Route path=":name" element={ <Category/> } />
+      </Route>
       <Route path="profile" element={ <Profile/> }>
         <Route path="edit" element={ <EditProfileForm/> } />
       </Route>
